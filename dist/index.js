2b911e513e4913ccc9d09d5093bb1e59a2bc5244
la <lucas.a.martins@ufv.br>
2017-10-24 20:10:45 -0200

    final msm

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const readline = require("readline");
 const helpers_1 = require("./helpers");
 const makeChildrenMoving = (node, from, to) => {
-    const board = node.board;
-    const newBoard = board.slice();
+    // Cria-se um clone do tabuleiro
+    const newBoard = node.board.slice();
+    // Faz o swap
     const aux = newBoard[from];
     newBoard[from] = newBoard[to];
     newBoard[to] = aux;
-    return { board: newBoard, children: [], father: node };
+    // Retorna na forma de Nodo da arvore
+    return { board: newBoard, father: node };
 };
 const getChildren = (node, restriction) => {
-    // get all possible children
     const indexOfEmpty = node.board.indexOf(0);
     let children = [];
     if (indexOfEmpty === 4) {
@@ -114,44 +117,73 @@ const getChildren = (node, restriction) => {
     return children;
 };
 const isSolved = (node) => {
+    // Para ver se esta solucionado fazemos uma comparacao
+    // entre o array que temos e o esperado.
     const solvedBoard = [1, 2, 3, 4, 5, 6, 7, 8, 0];
     return helpers_1.arraysEqual(node.board, solvedBoard);
 };
 const makeNewSolution = (node) => {
     let steps = [];
     let n = node;
+    // Enquanto houver um nodo pai adiciona-se ele a lista
+    // de passos.
     while (n.father) {
         steps = [n.board, ...steps];
         n = n.father;
     }
+    // O ultimo nodo nao tem pai mas precisa ser adicionado tambem.
     steps = [n.board, ...steps];
+    // Retorna o valor em formato de solucao
     return {
         length: steps.length - 1,
         finalBoard: node.board,
         steps
     };
 };
+let attemptCount = 0;
 const getSolution = (initialNode, restriction) => {
+    // Um array implementa a estrutura de dados fila.
     let queue = [initialNode];
     while (queue.length !== 0) {
+        // Pega o primeiro valor da fila e remove ele da fila.
         let node = queue.shift();
+        attemptCount++;
         if (!isSolved(node)) {
-            //Não é solução
-            node.children = getChildren(node, restriction);
-            node.children.forEach(n => queue.push(n));
+            // Não é solução. Portanto geramos todos os nodos filhos deste nodo
+            // e os colocamos no final da fila.
+            getChildren(node, restriction).forEach(n => queue.push(n));
         }
         else {
-            //É solução
+            // É solução, basta seguir os Nodos pais e contar quantos ate chegar
+            // no nodo inicial para saber os passos.
             return makeNewSolution(node);
         }
     }
+    // Se o programa limpou a fila significa que todas as possibilidades
+    // foram testadas e nenhuma serviu, portanto o problema nao tem solucao.
     throw Error("Solução não encontrada!");
 };
-const initialBoard = [1, 3, 0, 4, 2, 5, 7, 8, 6];
-//const initialBoard: Board = [1, 2, 3, 4, 5, 6, 0, 7, 8];
-let initialNode = {
-    board: initialBoard,
-    children: []
-};
-console.log(getSolution(initialNode, true));
+let initialBoard;
+const rl = readline.createInterface({
+    input: fs.createReadStream(process.argv[2])
+});
+let initialTime = Date.now();
+rl.on("line", (line) => {
+    initialBoard = line.split(", ").map(s => parseInt(s, 10));
+});
+rl.on("close", () => {
+    let initialNode = {
+        board: initialBoard
+    };
+    const solution = getSolution(initialNode, true);
+    console.log("Foram testadas " + attemptCount + " possibilidades.");
+    console.log("Resolvido em " + solution.length + " passos.");
+    console.log(solution.steps
+        .map((sol, index) => index === 0
+        ? "Inicio: " + JSON.stringify(sol)
+        : "Passo " + index + ": " + JSON.stringify(sol))
+        .join("\n"));
+    console.log("Estado final: " + JSON.stringify(solution.finalBoard));
+    console.log("Resolvido em " + (Date.now() - initialTime) / 1000 + " segundos.");
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+import * as readline from "readline";
 import { arraysEqual } from "./helpers";
 
 type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
@@ -6,7 +8,6 @@ type Board = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
 
 type TNode = {
   board: Board;
-  children: TNode[];
   father?: TNode;
 };
 
@@ -17,16 +18,17 @@ type Solution = {
 };
 
 const makeChildrenMoving = (node: TNode, from: Cell, to: Cell): TNode => {
-  const board = node.board;
-  const newBoard = board.slice() as Board;
+  // Cria-se um clone do tabuleiro
+  const newBoard = node.board.slice() as Board;
+  // Faz o swap
   const aux = newBoard[from];
   newBoard[from] = newBoard[to];
   newBoard[to] = aux;
-  return { board: newBoard, children: [], father: node };
+  // Retorna na forma de Nodo da arvore
+  return { board: newBoard, father: node };
 };
 
 const getChildren = (node: TNode, restriction: boolean): TNode[] => {
-  // get all possible children
   const indexOfEmpty = node.board.indexOf(0);
   let children: TNode[] = [];
   if (indexOfEmpty === 4) {
@@ -128,6 +130,8 @@ const getChildren = (node: TNode, restriction: boolean): TNode[] => {
 };
 
 const isSolved = (node: TNode): boolean => {
+  // Para ver se esta solucionado fazemos uma comparacao
+  // entre o array que temos e o esperado.
   const solvedBoard = [1, 2, 3, 4, 5, 6, 7, 8, 0];
   return arraysEqual(node.board, solvedBoard);
 };
@@ -135,11 +139,16 @@ const isSolved = (node: TNode): boolean => {
 const makeNewSolution = (node: TNode): Solution => {
   let steps: Board[] = [];
   let n = node;
+  // Enquanto houver um nodo pai adiciona-se ele a lista
+  // de passos.
   while (n.father) {
     steps = [n.board, ...steps];
     n = n.father;
   }
+  // O ultimo nodo nao tem pai mas precisa ser adicionado tambem.
   steps = [n.board, ...steps];
+
+  // Retorna o valor em formato de solucao
   return {
     length: steps.length - 1,
     finalBoard: node.board,
@@ -147,27 +156,59 @@ const makeNewSolution = (node: TNode): Solution => {
   };
 };
 
+let attemptCount = 0;
 const getSolution = (initialNode: TNode, restriction: boolean): Solution => {
+  // Um array implementa a estrutura de dados fila.
   let queue: TNode[] = [initialNode];
   while (queue.length !== 0) {
+    // Pega o primeiro valor da fila e remove ele da fila.
     let node = queue.shift();
+    attemptCount++;
     if (!isSolved(node)) {
-      //Não é solução
-      node.children = getChildren(node, restriction);
-      node.children.forEach(n => queue.push(n));
+      // Não é solução. Portanto geramos todos os nodos filhos deste nodo
+      // e os colocamos no final da fila.
+      getChildren(node, restriction).forEach(n => queue.push(n));
     } else {
-      //É solução
+      // É solução, basta seguir os Nodos pais e contar quantos ate chegar
+      // no nodo inicial para saber os passos.
       return makeNewSolution(node);
     }
   }
+  // Se o programa limpou a fila significa que todas as possibilidades
+  // foram testadas e nenhuma serviu, portanto o problema nao tem solucao.
   throw Error("Solução não encontrada!");
 };
 
-const initialBoard: Board = [1, 3, 0, 4, 2, 5, 7, 8, 6];
+let initialBoard: Board | undefined;
 
-let initialNode: TNode = {
-  board: initialBoard,
-  children: []
-};
+const rl = readline.createInterface({
+  input: fs.createReadStream(process.argv[2])
+});
 
-console.log(getSolution(initialNode, true));
+let initialTime = Date.now();
+rl.on("line", (line: string) => {
+  initialBoard = line.split(", ").map(s => parseInt(s, 10)) as Board;
+});
+
+rl.on("close", () => {
+  let initialNode: TNode = {
+    board: initialBoard
+  };
+  const solution = getSolution(initialNode, true);
+  console.log("Foram testadas " + attemptCount + " possibilidades.");
+  console.log("Resolvido em " + solution.length + " passos.");
+  console.log(
+    solution.steps
+      .map(
+        (sol, index) =>
+          index === 0
+            ? "Inicio: " + JSON.stringify(sol)
+            : "Passo " + index + ": " + JSON.stringify(sol)
+      )
+      .join("\n")
+  );
+  console.log("Estado final: " + JSON.stringify(solution.finalBoard));
+  console.log(
+    "Resolvido em " + (Date.now() - initialTime) / 1000 + " segundos."
+  );
+});
